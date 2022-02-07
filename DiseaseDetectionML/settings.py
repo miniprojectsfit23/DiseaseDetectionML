2a52eb2ad4b5748ae077f62a532f243e704558a1
Varun-Patkar <varunpatkar501@gmail.com>
2022-02-07 23:24:37 +0530

    Updated Static files"

--- DiseaseDetectionML/settings.py
+++ DiseaseDetectionML/settings.py
@@ -141,3 +141,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static/"),
 ]
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
